6ff5b93f032f506d17c5e669fd385bf0992e242d
Bruno <seletonurb@gmail.com>
2018-07-29 20:24:31 -0300

    Add Fetch Weatrher action and retrieve data from API (with axios)

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,15 @@
+import axios from 'axios';
+
 const WEATHER_FORECAST_API_KEY = "1636a271c9b9b1c4c9c14ef5403c2123";
+const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${WEATHER_FORECAST_API_KEY}`;
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
