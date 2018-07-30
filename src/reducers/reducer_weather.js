ee288d420ddb05a7831fe511edfcf42ae1808e69
Bruno <seletonurb@gmail.com>
2018-07-29 21:31:02 -0300

    Add city weather data to Redux state without mutating it

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,11 @@
-export default function(state = null, action) {
+import FETCH_WEATHER from "../actions/index";
+
+export default function(state = [ull], action) {
   // console.log("Action received ", action);
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // return state.concat([action.payload.data]); or spread operator:
+      return [action.payload.data, ...state];
+  }
   return state;
 }
