c9dac5848e11042e5fb419f23100f21ce22a9bdf
Bruno <seletonurb@gmail.com>
2018-07-29 21:02:08 -0300

    Dispatch Action Creator to Search Bar container

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const WEATHER_FORECAST_API_KEY = "1636a271c9b9b1c4c9c14ef5403c2123";
-const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${WEATHER_FORECAST_API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${WEATHER_FORECAST_API_KEY}`;
 
 export const FETCH_WEATHER = "FETCH_WEATHER";
 
