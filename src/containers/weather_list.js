94c690f33da4ad0a334eeb1aa65e484dbce0e0f5
Bruno <seletonurb@gmail.com>
2018-07-30 10:52:08 -0300

    Add more info (units, average) to Chart container

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -12,13 +12,13 @@ class WeatherList extends Component {
       <tr key={name}>
         <td>{name}</td>
         <td>
-          <Chart data={temps} color="orange" />
+          <Chart data={temps} color="orange" units="K" />
         </td>
         <td>
-          <Chart data={pressures} color="green" />
+          <Chart data={pressures} color="green"  units="hPa"/>
         </td>
         <td>
-          <Chart data={humidities} color="black" />
+          <Chart data={humidities} color="black"  units="%"/>
         </td>
       </tr>
     );
@@ -29,9 +29,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature(K)</th>
+            <th>Pressure(hPa)</th>
+            <th>Humidity(%)</th>
           </tr>
         </thead>
         <tbody>{this.props.weather.map(this.renderWeather)}</tbody>
